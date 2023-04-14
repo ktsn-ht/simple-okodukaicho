50dd4d74f7b331b3f574179a8008cbc576fd2cb9
ktsn <ktsn.0326.h.t@gmail.com>
2023-04-15 01:58:30 +0900

    front: ログインモーダルの実装

---
+++ front/src/components/LoginModal.tsx
@@ -0,0 +1,85 @@
+import { ChangeEvent, FC, memo, useState } from 'react';
+
+import {
+    Button, Checkbox, Flex, FormControl, FormLabel, Input, Link, Modal, ModalBody, ModalCloseButton,
+    ModalContent, ModalFooter, ModalOverlay, Stack
+} from '@chakra-ui/react';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export const LoginModal: FC<Props> = memo((props) => {
+  const { isOpen, onClose } = props;
+
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) =>
+    setPassword(e.target.value);
+  const onChangeShowPassword = (e: ChangeEvent<HTMLInputElement>) =>
+    setShowPassword(e.target.checked);
+
+  const onClickLogin = () => {
+    console.log(userId);
+    console.log(password);
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size={'sm'}
+      autoFocus={false}
+      motionPreset="slideInBottom"
+    >
+      <ModalOverlay />
+      <ModalContent pt={2} pb={2}>
+        <ModalCloseButton />
+        <ModalBody mx={4}>
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel>ユーザーID</FormLabel>
+              <Input value={userId} onChange={onChangeUserId} />
+            </FormControl>
+            <FormControl>
+              <FormLabel>パスワード</FormLabel>
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={onChangePassword}
+              />
+              <Checkbox
+                pt={2}
+                pl={2}
+                size={'sm'}
+                isChecked={showPassword}
+                onChange={onChangeShowPassword}
+              >
+                パスワードを表示する
+              </Checkbox>
+            </FormControl>
+          </Stack>
+        </ModalBody>
+        <ModalFooter>
+          <Flex w={'100vw'} align={'center'} justify={'center'}>
+            <Button
+              color={'white'}
+              bg={'cyan.600'}
+              mr={'1vw'}
+              fontSize={'sm'}
+              _hover={{ bg: 'cyan.500' }}
+              onClick={onClickLogin}
+            >
+              ログイン
+            </Button>
+          </Flex>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+});
--- front/src/pages/common/Header.tsx
+++ front/src/pages/common/Header.tsx
@@ -1,28 +1,40 @@
 import { FC, memo } from 'react';
 
-import { Button, Flex, Heading, Link } from '@chakra-ui/react';
+import { Button, Flex, Heading, Link, useDisclosure } from '@chakra-ui/react';
+
+import { LoginModal } from '../../components/LoginModal';
 
 export const Header: FC = memo(() => {
-  const onClickLogin = () => {};
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Flex
-      as={'nav'}
-      w={'100vw'}
-      bg={'cyan.800'}
-      color={'White'}
-      align={'center'}
-      justify={'space-between'}
-      padding={{ base: 3, md: 5 }}
-    >
-      <Flex as={'a'} align={'center'} _hover={{ cursor: 'pointer' }}>
-        <Heading as={'h1'} fontSize={{ base: 'md', md: 'lg' }}>
-          シンプルおこづかい帳
-        </Heading>
+    <>
+      <Flex
+        as={'nav'}
+        w={'100vw'}
+        bg={'cyan.800'}
+        color={'White'}
+        align={'center'}
+        justify={'space-between'}
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex as={'a'} align={'center'} _hover={{ cursor: 'pointer' }}>
+          <Heading as={'h1'} fontSize={{ base: 'md', md: 'lg' }}>
+            シンプルおこづかい帳
+          </Heading>
+        </Flex>
+        <Button
+          color={'white'}
+          bg={'cyan.600'}
+          mr={'1vw'}
+          fontSize={'sm'}
+          _hover={{ bg: 'cyan.500' }}
+          onClick={onOpen}
+        >
+          ログイン
+        </Button>
       </Flex>
-      <Button color={'white'} bg={'cyan.600'} mr={'1vw'} fontSize={'sm'}>
-        <Link onClick={onClickLogin}>ログイン</Link>
-      </Button>
-    </Flex>
+      <LoginModal isOpen={isOpen} onClose={onClose} />
+    </>
   );
 });
