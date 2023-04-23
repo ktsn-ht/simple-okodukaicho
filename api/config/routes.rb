8e1f29159be14e8450fea363bae0db47ce8bfd49
ktsn <ktsn.0326.h.t@gmail.com>
2023-04-24 01:15:32 +0900

    api: ユーザー登録 ・APIパス変更

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  post   'sign-up' => 'users#create'
+  post   'users'   => 'users#create'
 end
