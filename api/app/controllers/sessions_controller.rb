819c1812f60d2c317dff2b5ab51db5e4b3ae6a76
ktsn <ktsn.0326.h.t@gmail.com>
2023-04-23 18:16:32 +0900

    api: ログイン ・仮登録フラグをレスポンスに追加

--- api/app/controllers/sessions_controller.rb
+++ api/app/controllers/sessions_controller.rb
@@ -9,11 +9,22 @@ class SessionsController < ApplicationController
     @user = User.find_by(user_id: params[:user_id]) || User.find_by(email: params[:user_id])
     if @user&.authenticate(params[:password])
       update_jwt(@user.user_id)
-      render json: { message: 'login succeeded', user_id: @user.user_id, email: @user.email }, status: :ok
+      render json: create_ok_response, status: :ok
     else
       render json: { message: 'user not found' }, status: :unauthorized
     end
   end
 
   def destroy; end
+
+  private
+
+  def create_ok_response
+    {
+      message: 'login succeeded',
+      user_id: @user.user_id,
+      email: @user.email,
+      temporary: @user.temporary_flg
+    }
+  end
 end
