a62b71df6feb20d9ba8eba6671dd0a744a6938cc
ktsn <ktsn.0326.h.t@gmail.com>
2023-04-29 10:56:28 +0900

    api: コメント追加

--- api/app/models/category.rb
+++ api/app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :income_expenses, dependent: :destroy
 
+  # 初期収支カテゴリーの作成
   def self.initial_create(user)
     initial_expense_category_names = %w[住居費 水道光熱費 食費 日用品費 娯楽費 通信費 その他]
     initial_income_category_names = %w[給与 賞与 その他]
