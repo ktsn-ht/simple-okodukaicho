bec941c58cd2514fde8ace33901235105e5b91bd
ktsn <ktsn.0326.h.t@gmail.com>
2023-05-11 01:04:11 +0900

    db: 初期データ作成

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(user_id: 'user', password: 'p@ssword', email: 'test@example.com', temporary_flg: false)
+
+Category.initial_create(user)
+
+date = Time.zone.today
+beginning_of_month = date.beginning_of_month
+
+IncomeExpense.create(
+  amount: 200000, date: beginning_of_month + 23.days, user:, category: Category.find_by(name: '給与')
+)
+10.times do |i|
+  IncomeExpense.create(amount: 100, date:, memo: "test#{i}", user:, category: Category.find_by(name: '食費'))
+end
